4580da60b072ef5eb813e5744af36adb1bf6d4cb
Neel Contractor <neelcontractor7@gmail.com>
2023-08-10 11:33:04 +0530

    Update s3-notification-triggers.sh

--- aws-event-triggering/s3-notification-triggers.sh
+++ aws-event-triggering/s3-notification-triggers.sh
@@ -13,7 +13,7 @@ aws_region="us-east-1"
 bucket_name="neel-ultimate-bucket"
 lambda_func_name="s3-lambda-function"
 role_name="s3-lambda-sns"
-email_address="neelcontractor7@gmail.com"
+email_address="xyz@gmail.com"
 
 # Create IAM Role for the project
 role_response=$(aws iam create-role --role-name s3-lambda-sns --assume-role-policy-document '{
